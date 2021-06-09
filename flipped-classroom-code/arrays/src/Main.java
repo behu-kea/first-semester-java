402915263dee9b9b9683a93b4213cc9fcb941c90
Benjamin Hughes <behu@kea.dk>
2021-06-09 11:16:35 +0200

    asd

--- flipped-classroom-code/arrays/src/Main.java
+++ flipped-classroom-code/arrays/src/Main.java
@@ -1,8 +1,23 @@
 public class Main {
-    public static void main(String[] args) {
-        String[] lol = {"asd", "555"};
-        lol[0] = "aaa";
-        System.out.println(lol[0]);
+    public static String getLastElement(String[] arrayFromUser) {
+        return arrayFromUser[arrayFromUser.length - 1];
     }
+
+    public static void main(String[] args) {
+        String[] usernames = {"duckduck23", "heyThere", "asd", "benaa"};
+        String firstUsername = usernames[0];
+        //System.out.println(firstUsername);
+        String secondUsername = usernames[1];
+        //System.out.println(secondUsername);
+
+        //System.out.println(usernames.length);
+
+        //usernames[1] = "heyThere23";
+        //System.out.println(usernames[1]);
+        String[] names = {"peeter", "cam"};
+        String lastElement = getLastElement(names);
+        System.out.println("Last element is: " + lastElement);
+    }
+
 
 }
