2632109a37fccdcc8217cde88ab915a944595b67
Benjamin Hughes <behu@kea.dk>
2021-08-30 10:58:17 +0200

    asd

---
+++ lessons/week-02/classcode/src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        int test = 2;
+
+        int age;
+        double height;
+        age = 34;
+        height = 1.78;
+        double shoeSize = 38;
+        String name = "benjamin";
+
+    }
+}
