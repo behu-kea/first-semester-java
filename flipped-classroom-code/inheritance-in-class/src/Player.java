f635a3002252849fc73343fd2778c14d3d64e27f
Benjamin Hughes <behu@kea.dk>
2021-10-12 12:39:02 +0200

    asd

--- flipped-classroom-code/inheritance-in-class/src/Player.java
+++ flipped-classroom-code/inheritance-in-class/src/Player.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Player extends PlayerStats implements GameObject, SecondInterface {
     public String description;
 
@@ -8,6 +11,22 @@ public class Player extends PlayerStats implements GameObject, SecondInterface {
     @Override
     public void move() {
         int h = 0;
+
+        HashMap<String, Integer> usernameAges = new HashMap<String, Integer>();
+        usernameAges.put("peteTheBeat", 23);
+        usernameAges.put("mariannner", 29);
+
+        // setting a value at a specific key
+        usernameAges.put("typppi", 31);
+        System.out.println(usernameAges);
+        usernameAges.put("peteTheBeat", 99);
+        System.out.println(usernameAges);
+
+        // getting a value at a specific key
+        System.out.println(usernameAges.get("mariannner"));
+
+
+
     }
 
     public void die() {
